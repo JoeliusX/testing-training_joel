a8d930462c375706c14364353e07319d23f54b92
JoeliusX <169055973+JoeliusX@users.noreply.github.com>
2025-12-02 15:17:30 +0100

    feat: add validateArticleId

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -78,6 +78,9 @@ module.exports = class CartItem {
 
     #validateArticleId(articleId) {
         //TODO Implement this method
+        if (typeof articleId !== "number" || articleId < 1) {
+            throw new InvalidArticleIdException();
+        }
     }
 
     #validateQuantity(quantity) {
