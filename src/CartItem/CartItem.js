0c7958fd7deef1afe416a09ad77a6969effd70cb
JoeliusX <169055973+JoeliusX@users.noreply.github.com>
2025-12-02 15:15:39 +0100

    feat: add get total

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -57,6 +57,7 @@ module.exports = class CartItem {
 
     get total() {
         //TODO Implement this method
+        return this.#_quantity * this.#_price;
     }
     //endregion public methods
 
